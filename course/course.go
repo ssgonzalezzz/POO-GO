8a181b25742459bcb06d547b7c37c44132fda845
ssgonzalezz <samgon0909@gmail.com>
2021-10-13 21:18:54 -0400

    Added comments to the methods on package Courses

--- course/course.go
+++ course/course.go
@@ -2,6 +2,7 @@ package course
 
 import "fmt"
 
+// Estructura basica de los objetos de tipo Course. Pueden exportarse entre paquetes ya que su nombre inicia en mayusculas
 type Course struct {
 	Name    string
 	Price   float64
@@ -10,6 +11,7 @@ type Course struct {
 	Classes map[uint]string
 }
 
+//Imprime en consola las clases contenidas en un objeto de tipo Course
 func (c Course) PrintClasses() {
 	text := "Las clases del curso " + c.Name + " son: \n"
 	for _, v := range c.Classes {
@@ -18,6 +20,7 @@ func (c Course) PrintClasses() {
 	fmt.Println(text)
 }
 
+//Agrega un nuevo usuario al curso
 func (c *Course) AddUserId(id uint) []uint {
 	c.UserIDs = append(c.UserIDs, id)
 	return c.UserIDs
