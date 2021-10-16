758bcde579235fa950f26de8df09d5490f8b3203
ssgonzalezz <samgon0909@gmail.com>
2021-10-16 11:23:36 -0400

    New() method added in course package

--- course/course.go
+++ course/course.go
@@ -2,10 +2,11 @@ package course
 
 import (
 	"fmt"
+	"strconv"
 )
 
 // Estructura basica de los objetos de tipo Course. Pueden exportarse entre paquetes ya que su nombre inicia en mayusculas
-type Course struct {
+type course struct {
 	Name    string
 	Price   float64
 	IsFree  bool
@@ -13,14 +14,31 @@ type Course struct {
 	Classes map[uint]string
 }
 
+//Metodo constructor del objeto
+func New(name string, price float64) *course {
+	var isFree bool = false
+
+	if price == 0 {
+		isFree = true
+		fmt.Println("Precio = 0, se establecio como curso gratis")
+	}
+
+	return &course{
+		Name:   name,
+		Price:  price,
+		IsFree: isFree,
+	}
+
+}
+
 //Imprime en consola las clases contenidas en un objeto de tipo Course.
-func (c Course) PrintClasses() {
+func (c *course) PrintClasses() {
 
 	if c.Classes != nil && len(c.Classes) != 0 {
 		text := "Las clases del curso " + c.Name + " son: \n"
 
-		for i := 1; i <= len(c.Classes); i++ {
-			text = text + "- " + c.Classes[uint(i)] + "\n"
+		for i, v := range c.Classes {
+			text += strconv.Itoa(int(i)) + "- " + v + "\n"
 		}
 
 		fmt.Println(text)
@@ -31,7 +49,7 @@ func (c Course) PrintClasses() {
 }
 
 // Permite agregar una o mas clases nuevas al curso
-func (c *Course) AddClass(className ...string) {
+func (c *course) AddClass(className ...string) {
 	var l int
 	for _, v := range className {
 		l = len(c.Classes)
@@ -41,7 +59,7 @@ func (c *Course) AddClass(className ...string) {
 }
 
 //Agrega un nuevo usuario al curso
-func (c *Course) AddUserId(id uint) []uint {
+func (c *course) AddUserId(id uint) []uint {
 	c.UserIDs = append(c.UserIDs, id)
 	return c.UserIDs
 }
--- main.go
+++ main.go
@@ -8,30 +8,22 @@ import (
 
 func main() {
 
-	Go := course.Course{
-		Name:    "Go desde cero",
-		Price:   12.34,
-		IsFree:  false,
-		UserIDs: []uint{12, 32, 84},
-		Classes: map[uint]string{
-			1: "Introduccion",
-			2: "Estructuras",
-			3: "Mapas",
-		},
+	// Go.AddUserId(75)
+
+	// fmt.Println(Go.UserIDs)
+
+	// Go.AddClass("Prueba", "Prueba 2")
+
+	// Go.PrintClasses()
+
+	Go := course.New("Go desde cero", 15)
+	Go.UserIDs = []uint{12, 32, 84}
+	Go.Classes = map[uint]string{
+		1: "Introduccion",
+		2: "Estructuras",
+		3: "Mapas",
 	}
 
-	css := course.Course{
-		Name:    "CSS desde cero",
-		Classes: map[uint]string{},
-	}
-
-	Go.AddUserId(75)
-
-	fmt.Println(Go.UserIDs)
-
-	Go.AddClass("Prueba", "Prueba 2")
-
-	Go.PrintClasses()
-	css.PrintClasses()
+	fmt.Printf("%+v", Go)
 
 }
